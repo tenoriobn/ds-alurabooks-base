b669fcbdd8ca41b54abe1a2c65586cb339317f60
Bruno Tenório <tenoriobn@gmail.com>
2024-01-09 15:15:14 -0300

    Estilos com Styled

--- src/componentes/AbBotao/index.tsx
+++ src/componentes/AbBotao/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const BotaoEstilizado = styled.button`
+  background: #EB9B00;
+  padding: 16px 32px;
+  border: 2px solid #EB9B00;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #B87900;
+    border-color: #B87900;
+  }
+`;
 
 export const AbBotao = () => {
   return (
-    <button>Clique aqui!</button>
+    <BotaoEstilizado>Clique aqui!</BotaoEstilizado>
   )
 }
