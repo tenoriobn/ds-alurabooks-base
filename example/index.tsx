0a8c1d63364ce7227588c1b119e5307acb66bd58
Bruno Tenório <tenoriobn@gmail.com>
2024-01-09 15:42:01 -0300

    testando locamente

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { AbBotao } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <AbBotao />
     </div>
   );
 };
