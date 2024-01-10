a3b5e002fdf8fd9386d5483ec432af513e26e7c3
Bruno Tenório <tenoriobn@gmail.com>
2024-01-10 19:34:08 -0300

    evoluindo botão

--- src/componentes/AbBotao/index.tsx
+++ src/componentes/AbBotao/index.tsx
@@ -1,22 +1,39 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const BotaoEstilizado = styled.button`
-  background: #EB9B00;
-  padding: 16px 32px;
-  border: 2px solid #EB9B00;
-  color: #fff;
-  font-size: 20px;
-  cursor: pointer;
+export interface AbBotaoProps {
+    texto?: string
+    tipo?: 'primario' | 'secundario'
+    onClick?: () => void
+}
 
-  &:hover {
-    background-color: #B87900;
-    border-color: #B87900;
-  }
-`;
-
-export const AbBotao = () => {
-  return (
-    <BotaoEstilizado>Clique aqui!</BotaoEstilizado>
-  )
+const BotaoEstilizado = styled.button<AbBotaoProps>`
+    background: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#EB9B00' : '#FFF'};
+    padding: 16px 32px;
+    border: 2px solid #EB9B00;
+    color: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#FFF' : '#EB9B00'};
+    font-size: 20px;
+    cursor: pointer;
+    ${(props: AbBotaoProps) => props.tipo === 'primario' 
+        ? css`
+            &:hover {
+                background: #B87900;
+                border: 2px solid #B87900;
+            }
+        `
+        : css`
+        &:hover {
+            background: #FFF;
+            border: 2px solid #B87900;
+            color: #B87900;
+        }
+        `
+    }
+`
+export const AbBotao = ({ texto, onClick, tipo = 'primario' } : AbBotaoProps) => {
+    return (
+        <BotaoEstilizado onClick={onClick} tipo={tipo}>
+            {texto}
+        </BotaoEstilizado>
+    )
 }
