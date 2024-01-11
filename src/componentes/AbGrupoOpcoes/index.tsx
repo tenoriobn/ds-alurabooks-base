53798ed9361cfb61e0e31659345539fca296a84a
Bruno Tenório <tenoriobn@gmail.com>
2024-01-11 19:42:26 -0300

    adicionando estilo no componente AbGrupoOpcoes

--- src/componentes/AbGrupoOpcoes/index.tsx
+++ src/componentes/AbGrupoOpcoes/index.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+
+const SectionEstilizada = styled.section<{ selecionado: boolean }>`
+  width: 194px;
+  height: 88px;
+  background: ${props => props.selecionado ? 'linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%)' : '#FFFFFF'};
+  border: 1px solid;
+  border-color: ${props => props.selecionado ? '#002F52' : '#EB9B00'};
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  text-align: center;
+  margin: 10px;
+  font-family: sans-serif;
+  cursor: pointer;
+
+  header {
+    color: ${props => props.selecionado ? '#FFF' : '#EB9B00'};
+    font-size: 12px;
+    font-weight: 400;
+  }
+
+  strong {
+    color: ${props => props.selecionado ? '#FFF' : '#EB9B00'};;
+    font-size: 16px;
+    font-weight: 700;
+  }
+
+  footer {
+    color: ${props => props.selecionado ? '#FFF' : 'rgba(0, 0, 0, 0.54)'};;
+    font-size: 12px;
+    font-weight: 400;
+  }
+`;
 
 export const AbGrupoOpcoes = () => {
   return (
     <>
-      <section>
+      <SectionEstilizada selecionado={false}>
         <header>
           E-book
         </header>
@@ -15,9 +50,9 @@ export const AbGrupoOpcoes = () => {
         <footer>
           .pdf, .epub, .mob
         </footer>
-      </section>
+      </SectionEstilizada>
 
-      <section>
+      <SectionEstilizada selecionado={true}>
         <header>
           E-book
         </header>
@@ -29,9 +64,9 @@ export const AbGrupoOpcoes = () => {
         <footer>
           .pdf, .epub, .mob
         </footer>
-      </section>
+      </SectionEstilizada>
 
-      <section>
+      <SectionEstilizada selecionado={false}>
         <header>
           E-book
         </header>
@@ -43,7 +78,7 @@ export const AbGrupoOpcoes = () => {
         <footer>
           .pdf, .epub, .mob
         </footer>
-      </section>
+      </SectionEstilizada>
     </>
   );
 };
